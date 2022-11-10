5b1493547f20933c0931fded7f472a2f9ffbcd45
ssoloss <stevenssolos@gmail.com>
2022-11-10 10:46:53 -0500

    Init Commit

---
+++ FlamingoFinance/Pods/Target Support Files/Pods-FlamingoFinance-FlamingoFinanceUITests/Pods-FlamingoFinance-FlamingoFinanceUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlamingoFinance_FlamingoFinanceUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FlamingoFinance_FlamingoFinanceUITests
+@end
---
+++ FlamingoFinance/Pods/Target Support Files/Pods-FlamingoFinance/Pods-FlamingoFinance-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlamingoFinance : NSObject
+@end
+@implementation PodsDummy_Pods_FlamingoFinance
+@end
