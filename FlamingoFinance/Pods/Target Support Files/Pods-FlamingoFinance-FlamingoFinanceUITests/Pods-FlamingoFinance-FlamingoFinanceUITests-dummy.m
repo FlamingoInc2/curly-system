e246f3fe5927a67d92a822dab78eb24109bd83c3
ssoloss <stevenssolos@gmail.com>
2022-11-14 20:21:34 -0500

    Remake.

--- FlamingoFinance/Pods/Target Support Files/Pods-FlamingoFinance-FlamingoFinanceUITests/Pods-FlamingoFinance-FlamingoFinanceUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlamingoFinance_FlamingoFinanceUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FlamingoFinance_FlamingoFinanceUITests
-@end
--- FlamingoFinance/Pods/Target Support Files/Pods-FlamingoFinanceTests/Pods-FlamingoFinanceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlamingoFinanceTests : NSObject
-@end
-@implementation PodsDummy_Pods_FlamingoFinanceTests
-@end
