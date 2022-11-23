73290b782ebfd95a8bc7168be8718c5b91ea27db
ssoloss <stevenssolos@gmail.com>
2022-11-23 16:47:58 -0500

    SignupViewAdded

---
+++ Finance/Finance/SignUpViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SignUpViewController.swift
+//  Finance
+//
+//  Created by Steven Solo on 11/23/22.
+//
+
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    @IBAction func onSignup(_ sender: Any) {
+            let user = PFUser()
+            user.username = usernameField.text
+            user.password = passwordField.text
+
+        user.signUpInBackground{ (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSignSegue", sender: nil)
+            }
+            else {
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+        
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
