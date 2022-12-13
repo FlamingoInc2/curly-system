74911ab670528405f8c795d389c98a5516c71e39
ssoloss <stevenssolos@gmail.com>
2022-12-13 16:18:43 -0500

    Update Searchbar

--- Finance/Finance/SearchViewController.swift
+++ Finance/Finance/SearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import PolygonioSwift
+import SDWebImage
 
 class SearchViewController: UIViewController {
 
@@ -14,11 +15,65 @@ class SearchViewController: UIViewController {
     var receivedString = ""
     let polygon = PolygonioSwift.Client(key: "7iB1JD_bxYO6wj7lQdXLsuPDhcMUup3W")
     
+    @IBOutlet weak var CompName: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var PercentChange: UILabel!
+    @IBOutlet weak var OpenPrice: UILabel!
+    @IBOutlet weak var ClosePrice: UILabel!
+    @IBOutlet weak var HighPrice: UILabel!
+    @IBOutlet weak var MarketCap: UILabel!
+    @IBOutlet weak var Volume: UILabel!
+    @IBOutlet weak var Description: UILabel!
+    
+    @IBOutlet weak var compLogo: UIImageView!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print(receivedString)
+        
+        polygon.tickerDetails(symbol: "\(receivedString)") { [self]
+            (result: TickerDetailsResponse?, error) in print(result?.logo ?? "")
+            self.CompName.text = "\(result?.name ?? "Not Found")"
+            
+
+            compLogo.sd_setImage(with: URL(string: "\(result?.logo ?? "")"), placeholderImage: UIImage(named: "placeholder.png"))
+            MarketCap.text = "\(result?.marketcap ?? 0)"
+            Description.text = "\(result?.description ?? "")"
+            
+            
+        }
+        
+        polygon.tickerSnapshot(symbol: "\(receivedString)") { [self]  (result:TickerSnapshotResponse?, err) in
+                
+            if (result?.ticker.todaysChange ?? 1 < 1){
+                price.textColor = UIColor.red
+            }
+            else{
+                price.textColor = UIColor.green
+            }
+            price.text = "\(result?.ticker.min.VWAP ?? 0)"
+            PercentChange.text = "\(result?.ticker.todaysChangePerc ?? 0)"
+            OpenPrice.text = "\(result?.ticker.day.open ?? 0)"
+            ClosePrice.text = "\(result?.ticker.prevDay.close ?? 0)"
+            HighPrice.text = "\(result?.ticker.day.high ?? 0)"
+            Volume.text = "\(result?.ticker.day.volume ?? 0)"
+                
+        }
+        
+        
+        
+        
+        
+        
+        
+
     }
     
 
